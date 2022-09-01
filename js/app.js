edb0dd88db4a7a880af579b2e4aea940d5d1c009
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-01 21:51:31 +0600

    api loaded and display data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-const loadData = () => {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
+const loadData = (search) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayData(data.drinks));
 }
+const displayData = (drinks) => {
+    const drinksContainer = document.getElementById('drinks-container');
+    drinks.forEach(drink => {
+        const cocktailDiv = document.createElement('div');
+        cocktailDiv.classList.add('col')
+        cocktailDiv.innerHTML = `
+                    <div class="card" style="height: 600px;">
+                        <img src="${drink.strDrinkThumb}" class="card-img-top " alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${drink.strDrink}</h5>
+                            <p class="card-text">${drink.strInstructions}</p>
+                        </div>
+                    </div>
+        `;
+        drinksContainer.appendChild(cocktailDiv);
+    })
+}
+
+
+loadData('');
