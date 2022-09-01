10b8f9579b4ffb007980d6e37a9b038e904464b6
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-01 23:16:20 +0600

    search added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-const loadData = (search) => {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
+const loadData = (searchText) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayData(data.drinks));
 }
 const displayData = (drinks) => {
     const drinksContainer = document.getElementById('drinks-container');
+    drinksContainer.textContent = '';
     drinks.forEach(drink => {
         const cocktailDiv = document.createElement('div');
         cocktailDiv.classList.add('col')
@@ -21,6 +22,11 @@ const displayData = (drinks) => {
         drinksContainer.appendChild(cocktailDiv);
     })
 }
-
+const searchFood = () => {
+    const searchField = document.getElementById('drink-search');
+    const searchText = searchField.value;
+    loadData(searchText);
+    searchText.value = '';
+}
 
 loadData('');
